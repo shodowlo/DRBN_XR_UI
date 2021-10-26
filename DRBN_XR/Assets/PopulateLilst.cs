60876ad702cc6e07f9f3c034cde607e8b7bf713e
wong <wong.hua@gmail.com>
2021-10-26 16:52:08 +0200

    Finished self populating dropdown GUI element

---
+++ DRBN_XR/Assets/PopulateLilst.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class PopulateLilst : MonoBehaviour
+{
+    public Dropdown dropdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopulateList();
+    }
+
+    void MoleculeCensus() { 
+    }
+
+    void PopulateList (){
+
+        //test list
+        //List<string> molecules = new List<string>() {"toto","caca","pipoudou", "poupoupidou" };
+
+        List<string> molecules = new List<string>() { };
+
+        string[] moleculesprefab_dir = System.IO.Directory.GetDirectories("Assets/DRBN_STEAMVR/Prefab/Simulation/PDBs_To_Scale/");
+        //string[] moleculesprefab = System.IO.Directory.GetFiles("Assets/DRBN_STEAMVR/Prefab/Simulation/PDBs_To_Scale/");
+        //string[] moleculesprefab = System.IO.Directory.GetFiles("Assets /");
+        foreach (string dir in moleculesprefab_dir)
+        {
+            Debug.Log(dir + " directory");
+            string[] moleculesprefab = System.IO.Directory.GetFiles(dir,"i*.prefab");
+            foreach (string file in moleculesprefab)
+            {
+                {
+                    Debug.Log(file + " filename");
+                    molecules.Add(System.IO.Path.GetFileName(file));
+                    //Debug.Log(molecules);
+                }
+            }
+        }
+
+        dropdown.AddOptions(molecules);
+    }
+}
