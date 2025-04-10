ac1d37e00909aac776fbe98ebb8fb5ef876a2f78
desm0024 <lucas.desmarest@etudiant.univ-reims.fr>
2025-04-10 16:44:51 +0200

    idem

--- DRBN_XR/Assets/Script_Reset_Option.cs
+++ DRBN_XR/Assets/Script_Reset_Option.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Script_Reset_Option : MonoBehaviour
 {
+    public Slider UI_size;
+    //public 
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
