6eb834e44edf9f7c9b1be60d050fe510f77b960e
desm0024 <lucas.desmarest@etudiant.univ-reims.fr>
2025-04-10 16:37:09 +0200

    Optimisation element, panel reset option non fonctionnel

--- DRBN_XR/Assets/Option_Menu.cs
+++ DRBN_XR/Assets/Option_Menu.cs
@@ -1,29 +1,20 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class Option_menu : MonoBehaviour
+public class Navigation_Menu : MonoBehaviour
 {
-    [Header("Menu Buttons")]                    // Buttons of the menu
-    public List<GameObject> firstList;
+    public GameObject firstPanel;
+    public GameObject secondPanel;
 
-    [Header("Option Buttons")]                  // Buttons of the option menu
-    public List<GameObject> secondList;
-
-    public void PrintSecondList()               // Show the option menu
+    public void PrintSecondPanel()               // Show the option menu
     {
-        foreach (GameObject btn in firstList)
-            btn.SetActive(false);
-
-        foreach (GameObject btn in secondList)
-            btn.SetActive(true);
+        secondPanel.SetActive(true);
+        firstPanel.SetActive(false);
     }
 
-    public void BackToFirstList()               // return to the first panel
+    public void BackToFirstPanel()               // return to the first panel
     {
-        foreach (GameObject btn in secondList)
-            btn.SetActive(false);
-
-        foreach (GameObject btn in firstList)
-            btn.SetActive(true);
+        secondPanel.SetActive(false);
+        firstPanel.SetActive(true);
     }
 }
