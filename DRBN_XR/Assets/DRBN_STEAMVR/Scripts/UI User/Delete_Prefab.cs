4494d7a60e29ea7b891f14001faeaaceb09dd6d2
desm0024 <lucas.desmarest@etudiant.univ-reims.fr>
2025-04-16 11:46:22 +0200

    add collision and mouvement camera

--- DRBN_XR/Assets/DRBN_STEAMVR/Scripts/UI User/Delete_Prefab.cs
+++ DRBN_XR/Assets/DRBN_STEAMVR/Scripts/UI User/Delete_Prefab.cs
@@ -1,73 +1,3 @@
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.XR;
-
-// public class DeletePrefab : MonoBehaviour
-// {
-//     private InputDevice leftController;
-//     private InputDevice rightController;
-//     private bool previousButtonState = false;
-
-//     void Start()
-//     {
-//         InitializeControllers();
-//     }
-
-//     void InitializeControllers()
-//     {
-//         List<InputDevice> leftInputDevices = new List<InputDevice>();
-//         InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller, leftInputDevices);
-
-//         if (leftInputDevices.Count > 0)
-//         {
-//             leftController = leftInputDevices[0];
-//         }
-
-//         List<InputDevice> rightInputDevices = new List<InputDevice>();
-//         InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller, rightInputDevices);
-
-//         if (rightInputDevices.Count > 0)
-//         {
-//             rightController = rightInputDevices[0];
-//         }
-//     }
-
-//     void Update()
-//     {
-//         if (!rightController.isValid)
-//         {
-//             InitializeControllers();
-//             return;
-//         }
-
-//         bool isPressed = false;
-//         if (rightController.TryGetFeatureValue(CommonUsages.primaryButton, out isPressed) && isPressed && !previousButtonState)
-//         {
-//             DeletePointedObject();
-//         }
-
-//         previousButtonState = isPressed;
-//     }
-
-//     void DeletePointedObject()
-//     {
-//         if (leftController.isValid)
-//         {
-//             // Obtenir la position et la rotation du contrôleur gauche
-//             if (leftController.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position) &&
-//                 leftController.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation))
-//             {
-//                 Ray ray = new Ray(position, rotation * Vector3.forward);
-//                 if (Physics.Raycast(ray, out RaycastHit hit))
-//                 {
-//                     GameObject hitObject = hit.collider.gameObject;
-//                     Destroy(hitObject);
-//                 }
-//             }
-//         }
-//     }
-// }
-
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
@@ -77,6 +7,7 @@ public class DeletePrefab : MonoBehaviour
     private InputDevice leftController;
     private InputDevice rightController;
     private bool previousButtonState = false;
+    private GameObject leftGrabbedObject = null; // Variable pour suivre l'objet attrapé par le contrôleur gauche
 
     void Start()
     {
@@ -113,32 +44,52 @@ public class DeletePrefab : MonoBehaviour
         bool isPressed = false;
         if (rightController.TryGetFeatureValue(CommonUsages.primaryButton, out isPressed) && isPressed && !previousButtonState)
         {
-            DeletePointedObject();
+            DeleteGrabbedObject();
         }
 
         previousButtonState = isPressed;
+
+        // Vérifier si le contrôleur gauche attrape un objet
+        CheckForGrabbing(leftController, ref leftGrabbedObject);
     }
 
-    void DeletePointedObject()
+    void CheckForGrabbing(InputDevice controller, ref GameObject grabbedObject)
     {
-        if (leftController.isValid)
+        if (controller.isValid)
         {
-            if (leftController.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position) &&
-                leftController.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation))
+            bool gripPressed = false;
+            if (controller.TryGetFeatureValue(CommonUsages.gripButton, out gripPressed) && gripPressed)
             {
-                Ray ray = new Ray(position, rotation * Vector3.forward);
-                if (Physics.Raycast(ray, out RaycastHit hit))
+                if (grabbedObject == null)
                 {
-                    GameObject hitObject = hit.collider.gameObject;
-                    // destroy only object with tag molecule
-                    if (hitObject.CompareTag("molecule"))
+                    // Essayer d'attraper un objet
+                    if (controller.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position) &&
+                        controller.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation))
                     {
-                        Destroy(hitObject);
-                        Debug.Log("Objet avec le tag 'molecule' supprimé : " + hitObject.name);
-
+                        Ray ray = new Ray(position, rotation * Vector3.forward);
+                        if (Physics.Raycast(ray, out RaycastHit hit))
+                        {
+                            grabbedObject = hit.collider.gameObject;
+                            Debug.Log("Objet attrapé : " + grabbedObject.name);
+                        }
                     }
                 }
             }
+            else
+            {
+                // Relâcher l'objet si le bouton de préhension n'est pas pressé
+                grabbedObject = null;
+            }
+        }
+    }
+
+    void DeleteGrabbedObject()
+    {
+        if (leftGrabbedObject != null)
+        {
+            Destroy(leftGrabbedObject);
+            leftGrabbedObject = null;
+            Debug.Log("Objet attrapé par la manette gauche supprimé.");
         }
     }
 }
