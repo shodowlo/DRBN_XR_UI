aef33a6cbb998c8b50f09ac1387d37c2db8c4647
desm0024 <lucas.desmarest@etudiant.univ-reims.fr>
2025-04-30 16:38:53 +0200

    fix bug reset option

--- DRBN_XR/Assets/DRBN_STEAMVR/Scripts/UI_User/Script_Reset_Option.cs
+++ DRBN_XR/Assets/DRBN_STEAMVR/Scripts/UI_User/Script_Reset_Option.cs
@@ -8,13 +8,8 @@ public class Script_Reset_Option : MonoBehaviour
     public Interface_Size interfaceSize;
     public TimeControl timeControl;
 
-    void Start()
-    {
-    }
-
     public void ResetOption()
     {
-        // Set slider value to 0.5
         float defaultSliderValue = 0.5f;
         UI_size.value = defaultSliderValue;
 
@@ -24,9 +19,7 @@ public class Script_Reset_Option : MonoBehaviour
         // Reset the time scale to normal
         Time.timeScale = 1f;
 
-        // Allow scale update
-        interfaceSize.shouldUpdateScale = true;
-        interfaceSize.UpdateScale(UI_size.value);
+        interfaceSize.OnSliderReleased(UI_size.value);
 
         if (timeControl != null)
         {
